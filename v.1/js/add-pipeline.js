cd1cd45982ab78dd68143a1480a55911a33ae17a
Marques Lee <marques.lee@thoughtworks.com>
2019-04-10 16:58:16 -0700

    Add Job/Task section and such.

--- v.1/js/add-pipeline.js
+++ v.1/js/add-pipeline.js
@@ -1,5 +1,33 @@
 (function(global) {
+  "use strict";
+
   document.addEventListener("DOMContentLoaded", function() {
+    initMaterialSection();
+    initTaskTerminal(document.querySelector(".exec-editor"));
+    setTimeout(function() {
+      document.querySelector('.fillable input').focus();
+    }, 10);
+
+    initLinkStubs("a[href='#stub'],.run-ctl,.stage", [
+      "*chuckles* I'm not functional!",
+      "tee-hee, that tickles!",
+      "oh stop it, you.",
+      "and for my next trick: pull my finger!",
+      "all play and no work makes me...lazy.",
+      "et, voila.",
+      "yep. still ignoring you."
+    ]);
+  });
+
+  function initMaterialSection() {
+    var matType = document.getElementById("material-type");
+    var matField = closest(matType, ".field");
+
+    matField.setAttribute("data-type", matType.value);
+    matType.addEventListener("change", function() {
+      matField.setAttribute("data-type", matType.value);
+    });
+
     document.getElementById("test-connection").addEventListener("click", function(e) {
       var url = (document.querySelector("input[name='url']").value || "").trim();
       var result = document.getElementById("connection-result");
@@ -18,16 +46,38 @@
       });
     });
 
-    initLinkStubs("a[href='#stub'],.run-ctl,.stage", [
-      "*chuckles* I'm not functional!",
-      "tee-hee, that tickles!",
-      "oh stop it, you.",
-      "and for my next trick: pull my finger!",
-      "all play and no work makes me...lazy.",
-      "et, voila.",
-      "yep. still ignoring you."
-    ]);
-  });
+    function writeConnectionResult(el, message, fontawesome) {
+      var frag = document.createDocumentFragment();
+      frag.appendChild(document.createTextNode(message));
+      frag.appendChild(fontawesome);
+      empty(el).appendChild(frag);
+    }
+
+    function connectionTest(url, before, done, fail) {
+      if (!connectionTest.running) {
+        connectionTest.running = true;
+
+        if ("" === url) {
+          connectionTest.running = false;
+          return fail("URL is blank");
+        }
+
+        before();
+
+        if (url.match(/\bbad-url\b/i)) {
+          setTimeout(function() {
+            fail("Connection failed!");
+            connectionTest.running = false;
+          }, 400);
+        } else {
+          setTimeout(function() {
+            done();
+            connectionTest.running = false;
+          }, 400);
+        }
+      }
+    }
+  }
 
   function empty(el) {
     while (el.firstChild) {
@@ -43,47 +93,115 @@
     return el;
   }
 
-  function writeConnectionResult(el, message, fontawesome) {
-    var frag = document.createDocumentFragment();
-    frag.appendChild(document.createTextNode(message));
-    frag.appendChild(fontawesome);
-    empty(el).appendChild(frag);
-  }
+  function initTaskTerminal(term) {
+    var ENTER = 13;
+    var EDITOR = term.querySelector(".current-editor");
 
-  function connectionTest(url, before, done, fail) {
-    if (!connectionTest.running) {
-      connectionTest.running = true;
+    (function setup() {
+      document.body.addEventListener("click", function(e) {
+        saveCommand(EDITOR, true);
+      });
 
-      if ("" === url) {
-        connectionTest.running = false;
-        return fail("URL is blank");
+      term.addEventListener("keydown", function(e) {
+        if (e.target.matches(".current-editor")) {
+          keybindings(e);
+        }
+      });
+
+      term.addEventListener("click", function(e) {
+        if (e.target.matches(".current-editor")) {
+          e.stopPropagation(); // prevents need to click twice to focus caret in contenteditable on page load
+        }
+
+        if (e.target.matches(".task,.task *")) {
+          editCommand(e, EDITOR);
+        }
+      });
+
+      document.querySelector(".hideable").addEventListener("dblclick", function(e) {
+        e.currentTarget.classList.toggle("closed");
+        e.stopPropagation();
+        e.preventDefault();
+      });
+
+      // setTimeout(function() { EDITOR.focus(); }, 0);
+    })();
+
+    function editCommand(e, editEl) {
+      e.stopPropagation();console.log("c")
+      var el = closest(e.target, ".task");
+      saveCommand(editEl);
+      editEl.textContent = [ // be sure to use textContent to preserve newlines
+        el.querySelector(".cmd").textContent,
+        el.querySelector(".args").textContent
+      ].join(" ").trim();
+
+      el.parentNode.insertBefore(editEl, el);
+      el.parentNode.removeChild(el);
+      setTimeout(function(){ editEl.focus(); }, 0);
+    }
+
+    function saveCommand(el, moveToBottom) {
+      if (!el) {
+        el = EDITOR;
+      }
+      var line = el.textContent.trim();
+
+      if ("" !== line) {
+        var cmdStr = line, argStr = "";
+        var extractedCommand = false;
+        var args = Shellwords.split(line, function(token) {
+          if (!extractedCommand) {
+            // get the raw cmd string and argStr to
+            // preserve exactly what the user typed, so as
+            // to include whitespace and escape chars
+            cmdStr = line.slice(0, token.length).trim();
+            argStr = line.slice(token.length).trim();
+            extractedCommand = true;
+          }
+        });
+        var cmd = args.shift();
+        var saved = crel("pre", {class: "task"}, [
+          crel("span", {class: "cmd", "data-cmd": JSON.stringify(cmd)}, cmdStr),
+          crel("span", {class: "args", "data-cmd": JSON.stringify(args)}, argStr)
+        ]);
+
+        log("Parsed command that can be sent to server:", {
+          cmd: cmd,
+          args: args
+        });
+
+        el.parentNode.insertBefore(saved, empty(el));
       }
 
-      before();
-
-      if (url.match(/\bbad-url\b/i)) {
-        setTimeout(function() {
-          fail("Connection failed!");
-          connectionTest.running = false;
-        }, 400);
-      } else {
-        setTimeout(function() {
-          done();
-          connectionTest.running = false;
-        }, 400);
+      if (moveToBottom && el !== el.parentNode.lastChild) {
+        el.parentNode.appendChild(el)
       }
     }
-  }
 
-  // function asyncStub(fn, time, ontimeup) {
-  //   var running = false;
-  //   return function() {
-  //     if (running) { return; }
-  //     running = true;
-  //     fn.apply(this, arguments);
-  //     setTimeout()
-  //   }
-  // }
+    function keybindings(e) {
+      switch (e.which) {
+        case ENTER:
+          if (e.shiftKey) {
+          } else {
+            e.preventDefault();
+            e.stopPropagation();
+            saveCommand(e.target);
+          }
+          break;
+      }
+    }
+
+    function log(msg, obj) {
+      // var frag = document.createDocumentFragment();
+      // frag.appendChild(crel("h3", "Debugging messages"));
+      // if (arguments.length > 1) {
+      //   msg += " " + JSON.stringify(obj, null, 2);
+      // }
+      // frag.appendChild(document.createTextNode(msg));
+      // empty(document.getElementById("messages")).appendChild(frag);
+    }
+  }
 
   function initLinkStubs(selector, phrases) {
     var timer;
@@ -114,3 +232,67 @@
     }
   }
 })(window);
+
+// Gratuitously nabbed (and modified to work in browser) from https://raw.githubusercontent.com/jimmycuadra/shellwords/master/lib/shellwords.js
+(function(exports) {
+  var scan;
+
+  scan = function(string, pattern, callback) {
+    var match, result;
+    result = "";
+    while (string.length > 0) {
+      match = string.match(pattern);
+      if (match) {
+        result += string.slice(0, match.index);
+        result += callback(match);
+        string = string.slice(match.index + match[0].length);
+      } else {
+        result += string;
+        string = "";
+      }
+    }
+    return result;
+  };
+
+  exports.split = function(line, callback) {
+    var field, words;
+    if (line == null) {
+      line = "";
+    }
+    words = [];
+    field = "";
+    rawParsed = "";
+    scan(line, /\s*(?:([^\s\\\'\"]+)|'((?:[^\'\\]|\\.)*)'|"((?:[^\"\\]|\\.)*)"|(\\.?)|(\S))(\s|$)?/, function(match) {
+      var dq, esc, garbage, raw, seperator, sq, word;
+      raw = match[0], word = match[1], sq = match[2], dq = match[3], esc = match[4], garbage = match[5], seperator = match[6];
+      if (garbage != null) {
+        throw new Error("Unmatched quote");
+      }
+      rawParsed += match[0];
+      field += word || (sq || dq || esc).replace(/\\(?=.)/, "");
+      if (seperator != null) {
+        words.push(field);
+        if ("function" === typeof callback) {
+          callback(rawParsed);
+        }
+        rawParsed = "";
+        return field = "";
+      }
+    });
+    if (field) {
+      words.push(field);
+    }
+    return words;
+  };
+
+  exports.escape = function(str) {
+    if (str == null) {
+      str = "";
+    }
+    if (str == null) {
+      return "''";
+    }
+    return str.replace(/([^A-Za-z0-9_\-.,:\/@\n])/g, "\\$1").replace(/\n/g, "'\n'");
+  };
+
+})(window.Shellwords = {});
